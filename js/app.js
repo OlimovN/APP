88bf2665a2aa35cd7d73ca0d6be282b4528ec3e9
OlimovN <olimonurmuhammad711@gmail.com>
2024-03-16 16:26:26 +0500

    app

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const projects = [
+  {
+    title: "AkademNashr",
+    githubLink: "https://github.com/OlimovN/akadem-nashr",
+    vercelLink: "https://vercel.com/nurmukhammads-projects/akadem-nashr",
+  },
+  {
+    title: "Ansormed",
+    githubLink: "https://github.com/OlimovN/ansor-med",
+    vercelLink: "https://vercel.com/nurmukhammads-projects/akadem-nashr",
+  },
+  {
+    title: "My Cart",
+    githubLink: "https://github.com/OlimovN/user-card",
+    vercelLink: "https://user-card-delta.vercel.app/",
+  },
+  {
+    title: "Figma",
+    githubLink: "https://github.com/OlimovN/dars-ishi",
+    vercelLink: "https://vercel.com/nurmukhammads-projects/imtihon-figma",
+  },
+  {
+    title: "Uzbekistan",
+    githubLink: "https://github.com/OlimovN/uzbekistan",
+    vercelLink: "https://vercel.com/nurmukhammads-projects/uzbekistan",
+  },
+];
+
+const template = document.querySelector("template");
+const worksList = document.querySelector(".works__list");
+
+projects.forEach((project) => {
+  const clone = template.content.cloneNode(true);
+
+  const title = clone.querySelector(".works__list__title");
+  const linkGithub = clone.querySelector(".works__list__link__github");
+  const linkVercel = clone.querySelector(".works__list__link__vercel");
+
+  title.textContent = project.title;
+  linkGithub.href = project.githubLink;
+  linkVercel.href = project.vercelLink;
+
+  worksList.appendChild(clone);
+});
